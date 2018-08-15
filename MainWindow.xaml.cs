dd1e6e25793a2b983a8e249377dae961fadc8e87
Jesse Xu <35084397+jesse1xu@users.noreply.github.com>
2018-08-15 15:48:24 -0400

    Updated UI
    included settings tabs, output functionality, read-only functionality

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -23,11 +24,54 @@ namespace DiskReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string logDestinationString = "C:\\Users\\" + Environment.UserName + "\\Documents";
+        private string logicalDiskDestinationString = "C:\\ProgramData\\Unisys\\MCP Firmware\\Vdisks";
+        private string outputText = "";
+        private string outputType = "";
+        private bool readOnly = true;
         public MainWindow()
         {
             InitializeComponent();
 
-            ArrayList logicalList = Mapping.getAllLogicalDisksArray("C:\\Users\\XuJesseW\\Documents\\Programming");
+            if (!Mapping.IsAdministrator())
+            {
+                MessageBox.Show("Non-administrator user detected. Please run DiskReader as an administrator in order to view physical drives");
+            }
+
+            reinitialize();
+            labelBox.IsReadOnly = true;
+
+            filetypeBox.SelectedIndex = 0;
+        }
+
+        void treeViewItem_MouseLeftButtonUp(Object sender, RoutedEventArgs e)
+        {
+            TreeViewItem tvi = e.OriginalSource as TreeViewItem;
+            string header = (string)tvi.Header;
+            string identifier = header.Replace("Physical Drive ", "");
+            String text = Mapping.getLabelIdentifierOnlyNoLabels(identifier, logicalDiskDestinationString + "\\", readOnly);
+            labelInfo.Text = text;
+            outputText = text;
+            outputType = header;
+            if (text.Length > 0 && text != "")
+            {
+                lunName.Text = ((string)tvi.Header).ToUpper();
+            }
+            else
+            {
+                lunName.Text = "LUN";
+            }
+
+        }
+
+        void reinitialize()
+        {
+            if (trvMenu.Items.Count > 0)
+            {
+                trvMenu.Items.Clear();
+            }
+
+            ArrayList logicalList = Mapping.getAllLogicalDisksArray(logicalDiskDestinationString);
             ArrayList physicalList = Mapping.getAllPhysicalDrivesArray();
 
             TreeViewItem physicalDrives = new TreeViewItem()
@@ -50,7 +94,7 @@ namespace DiskReader
             }
             else
             {
-                logicalDisks.Items.Add(new TreeViewItem() { Header = "No Logical Disks Found" });
+                logicalDisks.Items.Add(new TreeViewItem() { Header = "None Found" });
             }
 
             if (physicalList.Count > 0)
@@ -62,49 +106,103 @@ namespace DiskReader
             }
             else
             {
-                physicalDrives.Items.Add(new TreeViewItem() { Header = "No Physical Drives Found" });
+                physicalDrives.Items.Add(new TreeViewItem() { Header = "None Found" });
             }
 
-            //MenuItem physicalDrives = new MenuItem() { Title = "Physical Drives" };
-            //MenuItem logicalDisks = new MenuItem() { Title = "Logical Disks" };
+            labelBox.Text = "Vol 1 Identifier\n" +
+                "Pack Serial Number\n" +
+                "Family Name\n" +
+                "System-Interchange Code\n" +
+                "Owner's Id\n" +
+                "Time Stamp\n" +
+                "Relative Index within Set\n" +
+                "Mask of Online Copies\n" +
+                "Recovery Option\n" +
+                "Auditing Marker\n" +
+                "MDPF (Spare Disk) Marker\n" +
+                "Vol 2 Identifier\n" +
+                "Initialization Date\n" +
+                "Initializing System\n" +
+                "Directory Link\n" +
+                "Integrity Flag";
 
-            //if(logicalList.Count>0)
-            //{
-            //    foreach (string s in logicalList)
-            //    {           
-            //        logicalDisks.Items.Add(new MenuItem() { Title = s });
 
-            //    }
-            //}
-            //else
-            //{
-            //    logicalDisks.Items.Add(new MenuItem() { Title = "No Logical Disks Found" });
-            //}
 
-            //if (physicalList.Count > 0)
-            //{
-            //    foreach (string s in physicalList)
-            //    {
-            //        physicalDrives.Items.Add(new MenuItem() { Title = s });
-            //    }
-            //}
-            //else
-            //{
-            //    physicalDrives.Items.Add(new MenuItem() { Title = "No Physical Drives Found" });
-            //}
-
+            logDestination.Text = logDestinationString;
+            logicalDiskDestination.Text = logicalDiskDestinationString;
 
             trvMenu.Items.Add(physicalDrives);
             trvMenu.Items.Add(logicalDisks);
 
+            // For write-mode
+            labelInfo.IsReadOnly = readOnly;
         }
 
-        void treeViewItem_MouseLeftButtonUp(Object sender, RoutedEventArgs e)
+        void logDestinationTextChanged(Object sender, TextChangedEventArgs args)
         {
-            TreeViewItem tvi = e.OriginalSource as TreeViewItem;
-            string identifier= ((string)tvi.Header).Replace("Physical Drive ","");
-            String text = Mapping.getLabelIdentifierOnly(identifier+".asd", "C:\\Users\\XuJesseW\\Documents\\Programming\\");
-            labelBox.Text = text;
+            logDestinationString = logDestination.Text;
+        }
+
+        void logicalDiskDestinationTextChanged(Object sender, TextChangedEventArgs args)
+        {
+            logicalDiskDestinationString = logicalDiskDestination.Text;         
+        }
+
+        void logOutputClicked(Object sender, RoutedEventArgs e)
+        {
+            string messageText;
+            if (outputType != "" && outputText != "")
+            {
+                messageText = Mapping.writeToTextFile(logDestinationString, outputType, outputText);
+            }
+            else
+            {
+                messageText = "Nothing to output.";
+            }
+            MessageBox.Show(messageText);
+        }
+
+        private void logBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+
+            dlg.Description = "Choose a file location for outputted logs.";
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                logDestinationString = dlg.SelectedPath;
+                logDestination.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void logicalDiskBrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog dlg = new System.Windows.Forms.FolderBrowserDialog();
+
+            dlg.Description = "Choose a file location for outputted logs.";
+
+            if (dlg.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                logicalDiskDestinationString = dlg.SelectedPath;
+                logicalDiskDestination.Text = dlg.SelectedPath;
+            }
+        }
+
+        private void readOnly_Checked(object sender, RoutedEventArgs e)
+        {
+            readOnly = true;
+        }
+
+        private void readOnly_Unchecked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("WARNING: enabling write-mode may compromise integrity of opened disks.");
+            readOnly = false;
+        }
+
+        private void reinitializeButton_Click(object sender, RoutedEventArgs e)
+        {
+            tabControl.SelectedIndex = 0;
+            reinitialize();
         }
     }
 
